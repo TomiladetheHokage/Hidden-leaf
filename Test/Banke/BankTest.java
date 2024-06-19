8a1b21880b7255a3a869f0a8bae6df5d8039c814
Oyebola <Oyebolatomilade@gmail.com>
2024-06-19 13:21:16 +0100

    BANK UPDATE

--- Test/Banke/BankTest.java
+++ Test/Banke/BankTest.java
@@ -5,25 +5,41 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankTest {
-    @Test
-    public void testNumberOfCustomersCanCount(){
-        Bank kuda = new Bank();
-        assertEquals(0, kuda.getNoOfCustomers());
-    }
 
     @Test
     public void testBankCanAddCustomer(){
         Bank kuda = new Bank();
-        kuda.addCustomer("1","tomi","12");
-        assertEquals(1, kuda.getNoOfCustomers());
+        kuda.addCustomer("1",12,"yemi");
+        assertEquals(1, kuda.noOfCustomers());
     }
 
     @Test
-    public void testBankCanWithdraw(){
+    public void testBankCanNotWithdrawFromNonExistingAccount(){
         Bank kuda = new Bank();
-        kuda.addCustomer("1","tomi","12");
-        kuda.withdrawFrom(500,"12");
-        assertEquals(0, kuda.balanceCheck("1"));
+        kuda.addCustomer("1",12,"tomi");
+        kuda.withdrawal(500,"1",11);
+
+        Account accountNotFound = kuda.findAccount(11);
+        assertEquals(null, accountNotFound);
+    }
+
+
+
+
+
+    @Test
+    public void testBankCanFindAccount(){
+        Bank kuda = new Bank();
+        kuda.addCustomer("1",12,"tomi");
+        Account foundAccount = kuda.findAccount(12);
+        assertEquals(12, foundAccount.getAccountNumber());
+        assertEquals("tomi", foundAccount.getaccountName());
+    }
+
+    @Test
+    public void testNumberOfCustomersCanCount(){
+        Bank kuda = new Bank();
+        assertEquals(0, kuda.noOfCustomers());
     }
 
 
--- src/Banke/Bank.java
+++ src/Banke/Bank.java
@@ -6,32 +6,53 @@ public class Bank {
     private ArrayList<Account> accounts;
     private int noOfCustomers;
 
-    Account account = new Account("123",12,"ola");
+    //Account account = new Account("123",12,"ola");
     public Bank(){
         accounts = new ArrayList<>();
         noOfCustomers = 0;
     }
 
-    public int getNoOfCustomers() {
-        return noOfCustomers;
-    }
-
-    public void addCustomer(String pin, String accountName, String accountNumber){
-        Account newAccount = new Account("123",12,"ola");
+    public void addCustomer(String pin, int accountNumber, String accountName) {
+        Account newAccount = new Account(pin, accountNumber, accountName);
         accounts.add(newAccount);
         noOfCustomers ++;
     }
 
+    public void withdrawal(int amount, String pin, int accountNumber) {
+        Account account = findAccount(accountNumber);
+        if (account != null) {
+            account.withdraw(amount, pin);
+        }
+        //else{
+        //    print("Account not found");
+        //}
 
-    public void withdrawFrom(int amount, String pin) {
-        Account account = new Account("1",1,"12");
-        account.withdraw(amount, pin);
     }
 
-    public int balanceCheck(String pin){
-        Account account = new Account("123",12,"ola");
-        return account.checkBalance(pin);
-       // return 0;
-        //return 0;
+    public void print(String input){
+        System.out.println(input);
     }
+
+    public Account findAccount(int accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber() == accountNumber) return account;
+            else print("Account not found");
+        }
+
+        return null;
+    }
+
+    public int noOfCustomers(){
+        return noOfCustomers;
+    }
+
+    public void depositTo (int accountNumber, int amount){
+        Account account = findAccount(accountNumber);
+        if (account != null){
+            account.deposit(amount);
+        }
+
+    }
+
+
 }
