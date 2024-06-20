46eb3cc56c03bc98456dfea8e18c28673a274233
Oyebola <Oyebolatomilade@gmail.com>
2024-06-20 22:54:21 +0100

    Completed bank. slight bug still

--- src/Banke/Atm.java
+++ src/Banke/Atm.java
@@ -12,7 +12,7 @@ public class Atm{
     }
 
     public static void print(String prompt){
-        System.out.print(prompt);
+        System.out.println(prompt);
     }
 
     public static void exitApplication(){
@@ -25,6 +25,7 @@ public class Atm{
         String pin = input("What is your pin? ");
         kuda.addCustomer(pin,name);
         print("Account succesfully created");
+        goToMainMenu();
     }
 
     public static void deposit(){
@@ -32,10 +33,82 @@ public class Atm{
         int accountNumber = Integer.parseInt(input("what is your account number? "));
         int amount = Integer.parseInt(input("How much do you want to deposit? "));
         kuda.depositTo(accountNumber, amount);
-
+        goToMainMenu();
     }
+
+    public static void withdraw(){
+        Bank kuda = new Bank();
+        int accountNumber = Integer.parseInt(input("what is your account number? "));
+        int amount = Integer.parseInt(input("How much do you wannt to withdra? "));
+        String pin = input("enter your pin");
+        kuda.withdrawal(amount,pin,accountNumber);
+        goToMainMenu();
+    }
+
+    public static void checkBalance(){
+        Bank kuda = new Bank();
+        int accountNumber = Integer.parseInt(input("What is your account number? "));
+        String pin = input("What is your pin? ");
+        kuda.checkBalanceFor(accountNumber, pin);
+        goToMainMenu();
+    }
+
+    public static void transfer(){
+        Bank kuda = new Bank();
+        int destinationAccount = Integer.parseInt(input("Enter the account number you want to send to: "));
+        int ownerAccountNumber = Integer.parseInt(input("Enter your account number: "));
+        int amount = Integer.parseInt(input("How much are you sending? "));
+        String pin = input("Enter your pin");
+        kuda.transfer(destinationAccount,ownerAccountNumber,amount,pin);
+        goToMainMenu();
+    }
+
+    public static void goToMainMenu(){
+        String menu = """
+                1. Create Account
+                2. Deposit
+                3. Withdraw
+                4. Check Balance
+                5. Go to main menu
+                6. Transfer
+                7. Exit 
+                """;
+        print(menu);
+        int userChoice = Integer.parseInt(input("please select: "));
+        //while (userChoice != 7) {
+            switch (userChoice) {
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 4:
+                    checkBalance();
+                    break;
+                case 5:
+                    goToMainMenu();
+                    break;
+                case 6:
+                    transfer();
+                    break;
+                case 7:
+                    exitApplication();
+                    break;
+                default:
+                    print("invalid choice please select again");
+        }
+        goToMainMenu();
+    }
+
     public static void main(String[] args){
-print("hi");
+        //Atm machine = new Atm();
+        //Bank kuda = new Bank();
+
+        goToMainMenu();
     }
 }
 
